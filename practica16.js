2957de85b460fdaccbf323a70e404d2adaa0920b
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2023-11-19 16:19:56 -0500

    Delete practica16.js

--- practica16.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-function verAño(añoNacimiento){
-    let resultado;
-    resultado=2023 - añoNacimiento;
-    alert("tu edad es:" + resultado);
-}
-let aN=Number(prompt("ingrese fecha de nacimiento"));
-verAño(aN);
